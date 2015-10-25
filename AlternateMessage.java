7511b8ef9210544ff92af2a8f6509bfcf35db111
yahsuanlin <yahsuanlin@live.com>
2015-10-25 21:30:38 +0800

    16.6

---
+++ AlternateMessage.java
@@ -0,0 +1,54 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class AlternateMessage extends JFrame {
+  //create a RotateMessagePanel instance for rotating two messages
+  private RotateMessagePanel panel = new RotateMessagePanel();
+  
+  public AlternateMessage() {
+	//place the message panel in the frame 
+	add(panel, BorderLayout.CENTER);
+    panel.setFocusable(true);
+  }
+
+  /** Main method */
+  public static void main(String[] args) {
+    JFrame frame = new AlternateMessage();
+    frame.setTitle("AlternateTwoMessage");
+    frame.setSize(200,100);
+    frame.setLocationRelativeTo(null); // Center the frame
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+  }
+
+  class RotateMessagePanel extends JPanel {
+	private String msg1 = "Java is fun";
+    private String msg2 = "Java is powerful";
+    private String message = msg1; // Message to display	
+	
+	/** Contruct a panel to rotate two msgs */
+    public RotateMessagePanel() {
+		addMouseListener(new MouseAdapter() {
+			/** Handle mouse-clicked event */
+			public void mouseClicked(MouseEvent e) {
+			  if(message.equals(msg1))
+				message = msg2;
+			  else 
+				message = msg1;	
+				
+			repaint();	
+        }
+      });
+    }
+
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g); 
+		FontMetrics fm = g.getFontMetrics();
+		int sw = fm.stringWidth(message);
+		int sa = fm.getAscent();
+		int x = getWidth()/2-sw/2;
+		int y = getHeight()/2+sa/2; g.drawString(message, x, y);
+	}
+  }
+}
